56c66c3c67e211b75c884f06dc54c9d71e91be25
JuH33 <julien.boyer49@orange.fr>
2017-08-30 18:07:18 +0200

    add roles getter

--- src/main/com/mapotempo/fleet/core/model/User.java
+++ src/main/com/mapotempo/fleet/core/model/User.java
@@ -5,6 +5,8 @@ import com.couchbase.lite.Document;
 import com.mapotempo.fleet.core.base.MapotempoModelBase;
 import com.mapotempo.fleet.core.base.DocumentBase;
 
+import java.util.ArrayList;
+import java.util.List;
 /**
  * Company.
  */
@@ -14,6 +16,7 @@ public class User extends MapotempoModelBase {
     // MAPOTEMPO KEY
     public static final String USER = "user";
     public static final String COMPANY_ID = "company_id";
+    public static final String ROLES = "roles";
 
     public User(Database database) {
         super(database);
@@ -29,5 +32,9 @@ public class User extends MapotempoModelBase {
 
     public String getCompanyId() {
         return (String)getProperty(COMPANY_ID, "No company id found");
+    }
+
+    public List<String> getRoles() {
+        return (ArrayList<String>)getProperty(ROLES, new ArrayList<String>());
     }
 }
