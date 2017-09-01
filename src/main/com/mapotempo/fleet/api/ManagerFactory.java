9f87d3700c30d3dc5ce0de231a445e3810dada24
FILLAU Jean-Maxime <jean-maxime.fillau@mapotempo.com>
2017-09-01 15:09:41 +0200

    Adding Mapotempo's copyright in all source files.
    Signed-off-by: FILLAU Jean-Maxime <jean-maxime.fillau@mapotempo.com>

--- src/main/com/mapotempo/fleet/api/ManagerFactory.java
+++ src/main/com/mapotempo/fleet/api/ManagerFactory.java
@@ -1,13 +1,37 @@
+/*
+ * Copyright © Mapotempo, 2017
+ *
+ * This file is part of Mapotempo.
+ *
+ * Mapotempo is free software. You can redistribute it and/or
+ * modify since you respect the terms of the GNU Affero General
+ * Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Mapotempo is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the Licenses for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Mapotempo. If not, see:
+ * <http://www.gnu.org/licenses/agpl.html>
+ */
+
 package com.mapotempo.fleet.api;
+
 
 import com.couchbase.lite.Context;
 import com.mapotempo.fleet.core.MapotempoFleetManager;
 
+/**
+ * <h1>ManagerFactory</h1>
+ * <p>{@link ManagerFactory} is the entry point for the mapotempo fleet java client.</p>
+ */
 public class ManagerFactory {
 
     /**
-     * TODO
-     * @param context
+     * A default no connected manager.
+     * @param context Application context
      * @return return a {@link MapotempoFleetManagerInterface}
      */
     public static MapotempoFleetManagerInterface getDefaultManager(Context context) {
@@ -34,6 +58,6 @@ public class ManagerFactory {
      * @param url
      */
     public static void getManager(Context context, String user, String password, MapotempoFleetManagerInterface.OnServerConnexionVerify onServerConnexionVerify, String url) {
-        new MapotempoFleetManager(context, user, password, onServerConnexionVerify, url);
+        MapotempoFleetManager mapotempoFleetManager = new MapotempoFleetManager(context, user, password, onServerConnexionVerify, url);
     }
 }
