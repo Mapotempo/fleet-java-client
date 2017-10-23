fc5c0e59d8d7e6e693e907f60c022d943611b3d3
FILLAU Jean-Maxime <jean-maxime.fillau@mapotempo.com>
2017-10-23 16:17:26 +0200

    Change Date format.
    Signed-off-by: FILLAU Jean-Maxime <jean-maxime.fillau@mapotempo.com>

--- src/main/com/mapotempo/fleet/utils/DateHelper.java
+++ src/main/com/mapotempo/fleet/utils/DateHelper.java
@@ -41,7 +41,7 @@ public class DateHelper {
     // ####################################
     // ISO8601 date String
     // ####################################
-    private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX");
+    private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
 
     public static String toStringISO8601(Date value) {
         return sdf.format(value);
